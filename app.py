44be2028fe2c21f00230c8cdb4b4a4c319ba281d
Omar Gastelum <ogastelum@collabtime.co>
2022-06-09 05:10:08 -0700

    Fixes #5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -38,9 +38,12 @@ def page_not_found(e):
 def name():
     name = None
     form = NamerForm()
-    if form.validate_on_submit:
+    
+    if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash('Form submitted successfully!')
+        
     return render_template("name.html",
         name = name,
         form = form)
